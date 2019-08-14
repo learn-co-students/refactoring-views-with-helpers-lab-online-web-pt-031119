4acb0ab951557f35d8da88ae173867a26235e000
“Mark <mkanko66@gmail.com>
2019-08-13 20:50:58 -0700

    completed refactoring-views-with-helpers-lab

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,12 @@
 module ArtistsHelper
+
+    def display_artist(song) 
+        if song.artist 
+            # return a link to artist's show page.
+            link_to song.artist.name, artist_path(song.artist)  
+        else
+            # return a link to song's edit page, with link text "Add Artist".
+            link_to "Add Artist", edit_song_path(song) 
+        end 
+    end 
 end
